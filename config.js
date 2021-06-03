0573d1259297ea3ee36c3e0f64a0411d479772a8
tranvanty0711 <84216939+tranvanty0711@users.noreply.github.com>
2021-06-03 14:25:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     introTitle: 'Hợp à !',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trái đất vốn lạ thường,
+     mà sao anh cứ đi nhầm đường.
+    Lạc vào tim em lẻ loi,
+     đằng sau chữ yêu đây là thương.',
     btnIntro: 'Nhấn nút này nè!',
     title: 'Em biết anh thích em mà đúng không?',
     desc: 'Và anh nghĩ em cũng dị đúng không?',
