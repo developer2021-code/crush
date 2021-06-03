1757873168c6506e66407d44852382f7203e44c2
tranvanty0711 <84216939+tranvanty0711@users.noreply.github.com>
2021-06-03 22:04:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 const CONFIG = {
     introTitle: 'Hợp à !',
-    introDesc: `Anh yêu em bao lâu thật ra không cần thiết,
-     anh yêu em ra sao người ta không cần biết.
-    Anh thì không biết nói những lời hoa mỹ, anh chỉ có sự chân thành.`,
-    btnIntro: 'Em đang cười anh hả! Nhấn nút này đi!',
+    introDesc: `Sao 20 mấy năm nay em trốn anh kỹ dữ kkk`,
+    btnIntro: 'Em đang cười anh hả? Nhấn nút này đi !',
     title: 'Em biết anh thích em mà đúng không?',
-    desc: 'Và anh nghĩ em cũng dị hặm :D ?',
-    btnYes: 'Đúng vậy <3',
-    btnNo: 'Không, Anh Tý :3',
-    question:'Em nói gì với anh đi',
+    desc: 'Và anh mong em cũng dị ?',
+    btnYes: 'Đúng vậy',
+    btnNo: 'Không phải đâu Anh Tý',
+    question:'Em nhắn gì với anh đi',
     btnReply: 'Gửi anh tý',
-    reply: 'Hợp dễ thương, xinh lạy trời!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
+    reply: 'Hợp dễ thương lạy trời!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
     mess: 'Anh biết mà haha',
-    messDesc: 'Cho phép anh được quan tâm em mỗi ngày nha!',
+    messDesc: 'Nghe nhạc hỉ !',
     btnAccept: 'Ok luôn <3',
     messLink: 'https://www.youtube.com/watch?v=z35r-OeqLgQ' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
