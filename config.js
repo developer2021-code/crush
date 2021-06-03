a289f2029e54e2f0d6680794a8e8c44c29445a36
tranvanty0711 <84216939+tranvanty0711@users.noreply.github.com>
2021-06-03 16:41:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ const CONFIG = {
     introDesc: `Anh yêu em bao lâu thật ra không cần thiết,
      anh yêu em ra sao người ta không cần biết.
     Anh yêu vì mắt môi, yêu vì tiếng cười và
-    yêu trong suy tư, nhiều hơn trong ngòi viết.`,
+    yêu trong suy tư, nhiều hơn trong ngòi viết. 
+    Anh nghèo những lời hoa mỹ, anh chỉ giàu ở sự chân thành.`,
     btnIntro: 'hihi. Nhấn nút này nè!',
     title: 'Em biết anh thích em mà đúng không?',
     desc: 'Và anh nghĩ em cũng dị đúng không?',
@@ -15,5 +16,5 @@ const CONFIG = {
     mess: 'Anh biết mà haha',
     messDesc: 'Cho phép anh được quan tâm em mỗi ngày nha!',
     btnAccept: 'Ok luôn <3',
-    messLink: 'https://www.youtube.com/watch?v=vDfkqjhtiCM' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.youtube.com/watch?v=z35r-OeqLgQ' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
