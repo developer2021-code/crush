155daf73576c8de96e22a026ef9fb025bf277cbd
developer2021-code <85425610+developer2021-code@users.noreply.github.com>
2021-06-07 19:25:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@ const CONFIG = {
     introDesc: `Đừng cục súc với anh nữa nha
     btnIntro: 'Nhấn dô cái này đi
     title: 'Anh biêt anh sai rồi,bé cũng bỏ qua cho anh rồi mà đúng không.
-    Mình vẩn như trước kia,vẫn vui vẽ với nhau,đừng cố tạo khoảng cach n
+    Mình vẩn như trước kia,vẫn vui vẽ với nhau,đừng cố tạo khoảng cach nữa.
     desc: Bé nha
 
     btnYes: 'Dạ
     btnNo: 'Không
     question:'Em nhắn gì với anh đi,nhắn ra mess ă
     btnReply: 'Gửi anh Phát
-    reply: 'Hợp dễ thương lạy trời!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
-    mess: 'Anh biết mà haha',
-    messDesc: 'Nghe nhạc hỉ !',
-    btnAccept: 'Ok luôn <3',
-    messLink: 'https://www.youtube.com/watch?v=z35r-OeqLgQ' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: ' Má Phệ dễ thương!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
+    mess: 'Cám ơn nha haha',
+    messDesc: 'Nghe nhạc đi !',
+    btnAccept: 'Ok Nè
+    messLink: 'https://www.youtube.com/watch?v=zEWSSod0zTY
 }
