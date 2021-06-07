728d68a163b9aef0f803e10cb46267820376558a
developer2021-code <85425610+developer2021-code@users.noreply.github.com>
2021-06-07 19:37:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     introTitle: 'Má Phệ À  
     introDesc: `Đừng cục súc với anh nữa nha
-    btnIntro: 'Nhấn dô cái này đi
+    btnIntro: 'Nhấn dô cái này đi'
     title: 'Anh biêt anh sai rồi,bé cũng bỏ qua cho anh rồi mà đúng không.
-    Mình vẩn như trước kia,vẫn vui vẽ với nhau,đừng cố tạo khoảng cach nữa.
-    desc: Bé nha
+    Mình vẩn như trước kia,vẫn vui vẽ với nhau,đừng cố tạo khoảng cach nữa.'
+    desc: 'Bé nha'
 
-    btnYes: 'Dạ
-    btnNo: 'Không
-    question:'Em nhắn gì với anh đi,nhắn ra mess ă
-    btnReply: 'Gửi anh Phát
+    btnYes: 'Dạ'
+    btnNo: 'Không'
+    question:'Em nhắn gì với anh đi,nhắn ra mess ă'
+    btnReply: 'Gửi anh Phát'
     reply: ' Má Phệ dễ thương!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
     mess: 'Cám ơn nha haha',
     messDesc: 'Nghe nhạc đi !',
-    btnAccept: 'Ok Nè
-    messLink: 'https://www.youtube.com/watch?v=zEWSSod0zTY
+    btnAccept: 'Ok Nè'
+    messLink: 'https://www.youtube.com/watch?v=zEWSSod0zTY'
 }
