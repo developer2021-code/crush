cc1f6dd0f80417b1ddf37233aa49903a0728efdb
developer2021-code <85425610+developer2021-code@users.noreply.github.com>
2021-06-07 19:20:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 const CONFIG = {
-    introTitle: 'Hợp à !',
-    introDesc: `Sao 20 mấy năm nay em trốn anh kỹ dữ kkk`,
-    btnIntro: 'Em đang cười anh hả? Nhấn vào đi !',
-    title: 'Em biết anh thích em mà đúng không?',
-    desc: 'Và anh mong em cũng dị ?',
-    btnYes: 'Đúng vậy',
-    btnNo: 'Không phải đâu Anh Tý',
-    question:'Em nhắn gì với anh đi',
-    btnReply: 'Gửi anh tý',
+    introTitle: 'Má Phệ À  
+    introDesc: `Đừng cục súc với anh nữa nha
+    btnIntro: 'Nhấn dô cái này đi
+    title: 'Anh biêt anh sai rồi,bé cũng bỏ qua cho anh rồi mà đúng không.
+    Mình vẩn như trước kia,vẫn vui vẽ với nhau,đừng cố tạo khoảng cach n
+    desc: Bé nha
+
+    btnYes: 'Dạ
+    btnNo: 'Không
+    question:'Em nhắn gì với anh đi,nhắn ra mess ă
+    btnReply: 'Gửi anh Phát
     reply: 'Hợp dễ thương lạy trời!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
     mess: 'Anh biết mà haha',
     messDesc: 'Nghe nhạc hỉ !',
