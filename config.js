d18e690e95396c09b2747aa1ef8da357e234da61
developer2021-code <85425610+developer2021-code@users.noreply.github.com>
2021-06-07 20:16:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introTitle: 'Má Phệ À !',
     introDesc: `Đừng cục súc với anh nữa nha`,
     btnIntro: 'Nhấn dô cái này đi',
-    title: 'Anh biêt anh sai rồi,bé cũng bỏ qua cho anh rồi mà đúng không. Mình vẩn như trước kia,vẫn vui vẽ với nhau, biết là rất khó để bé có lại cảm giác,nhưng 1 thời gian sau rồi sẽ khác mà. Đừng cố tạo khoảng cách nữa.',
+    title: 'Anh biêt anh sai rồi,bé cũng bỏ qua cho anh rồi mà đúng không. Mình vẩn như trước kia,vẫn vui vẽ với nhau,  biết là rất khó để bé có lại cảm giác,nhưng 1 thời gian sau rồi sẽ khác mà. Đừng cố tạo khoảng cách nữa.',
     desc: 'Bé nha',
     btnYes: 'Dạ',
     btnNo: 'Không',
